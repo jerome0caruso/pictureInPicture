55188945a32c258e100ca5a816d9f367b9d4400c
jerome0caruso <jerome0caruso@myjavascriptprojects.com>
2020-10-01 14:33:34 -0500

    almost complete

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const button = document.getElementById('button');
 
 async function selectMediaStream() {
     try {
+        //waiting to assign until user selects screem/window to share
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        //passing that data into video object as its source
         videoElement.srcObject = mediaStream;
+        //when video loads meta data then play
         videoElement.onloadloadedmetadata = () => {
             videoElement.play();
         }
@@ -12,5 +15,12 @@ async function selectMediaStream() {
         console.log("Errorrr");
     }
 }
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+    //start picture in picture
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+})
 
 selectMediaStream();
